5cb936a74dc7e7ae8989287369d3e911e53e7f4b
ANKUR RAWAT <106335896+ankurrawat043@users.noreply.github.com>
2022-12-01 14:43:47 +0530

    for git hub

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import React , { useState } from 'react';
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Alert from './components/Alert';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
-import {
+/*import {
   BrowserRouter as Router,
   
   Route,
   Link,
   Switch
-} from "react-router-dom";
+} from "react-router-dom";*/
 
 
  function App() {
@@ -45,30 +45,34 @@ import {
     }
   }
   return (
+   
     <>
-    <Router>
+    
     
    <NavBar title="TextUtils" about="About TextUtils" mode={mode} toggleMode={toggleMode}/>
    <Alert alert={alert}/>
    <div className='container my-3'>
-   <Switch>
-          <Route  path="/about">
-            <About />
-          </Route>
-          <Route  path="/">
-         <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>  
-          </Route>
-    </Switch>      
-          
+  
+
+    <TextForm showAlert={showAlert} heading="Enter your text to analyze below" mode={mode}/>       
         
     
    </div>
    
     
-    </Router>
+   
     </>
   );
 }
 
 export default App;
-
+//<Router>
+ /*<Switch>
+          //<Route  path="/about">
+            <About />
+          </Route>
+          <Route  path="/">
+          
+         // </Route>
+    </Switch> */
+    // </Router>
--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <h1>About Us</h1>
-      <h2>asdaasdasdsaasdasdsad</h2>
-      <p>asdasdasdasadasdasdasdasdasdasdasdasdasdascvxcvxcvsdvsvjs,dgfajfhgncdbmzcbdkfdbcnbdm</p>
     </div>
   )
 }
+
+
+
+
